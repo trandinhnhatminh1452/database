2de86e2c529ed78f51ff6de9b36f7c1eb00dceb9
Trần Đình Nhật Minh <138916400+trandinhnhatminh1452@users.noreply.github.com>
2024-08-27 22:33:22 +0700

    Update insert_coach.sql

--- postgreSQL/procedures/insert_coach.sql
+++ postgreSQL/procedures/insert_coach.sql
@@ -17,26 +17,45 @@ DECLARE
     coach_count INTEGER;
 	overlap_count INTEGER;
 BEGIN
+
+	select count(*) into coach_count
+	from _flms.coach_team 
+	where _flms.coach_team.club_name= _club_name;
+	
+	if coach_count >= 15 then 
+	raise exception ' club coach limit reached (15 coaches). Cannot insert more coaches for %',club_name;
+	end if;
+	
 	--check if coach already exist in coaches table
 	Select coach_id into _coach_id
-	from _flms.coachs
+	from _flms.coaches
 	where coach_name = _coach_name and nationality = _nationality and date_of_birth = _date_of_birth;
 	
 	if _coach_id is null then
 		-- Insert coach into coaches table
 		INSERT INTO _flms.coaches (coach_name, nationality, date_of_birth) 
-		VALUES (_coach_name, nationality, date_of_birth)
+		VALUES (_coach_name, _nationality, _date_of_birth)
 		RETURNING coach_id INTO _coach_id;
 
 		-- Insert coach into coach_team table
 		INSERT INTO _flms.coach_team (club_name, coach_id, "begin" ,"end") 
 		VALUES (_club_name, _coach_id, begin_date, end_date);
-	else
+	
+		
+		else
+		select count(*) into overlap_count
+		from _flms.coach_team
+		where coach_id = _coach_id
+		and (begin_date, end_date) overlaps ("begin", "end");
+		
+		if overlap_count>0 then
+		raise exception 'contract  already exist for coach % on this period.', _coach_id;
+	end if;
+	
 		-- Insert coach and club into coach_team table
 		INSERT INTO _flms.coach_team (club_name, coach_id, "begin" ,"end") 
 		VALUES (_club_name, _coach_id, begin_date, end_date);
 	end if;
-
 END;
 $BODY$;
 ALTER PROCEDURE _flms.insert_coach(character varying, character varying, character varying, date, date, date)
